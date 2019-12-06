6fc419b642e661f04a8e9a2449a3eed3463e95e4
Andréa Christophe <andrea.christophe@g-mail.fr>
2019-12-06 16:08:31 +0100

    First Commit

---
+++ src/main/java/bowling/miage/App.java
@@ -0,0 +1,13 @@
+package bowling.miage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
